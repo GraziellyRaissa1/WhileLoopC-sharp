ff43e9242b4c4da1aa8867fed735c60b905762d0
GraziellyRaissa1 <grazyraissa7@gmail.com>
2024-06-15 00:40:34 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopC_sharp
+{
+    internal class Exemplo01
+    {
+        public static void Main(string[] args)
+        {
+            int     A = 0;
+            while (A < 5)
+            {
+                Console.WriteLine(A);
+                A++;
+        }    }
+    }
+}
